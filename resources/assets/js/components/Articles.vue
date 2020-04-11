169cc2c65e76f6f7fea031e4329c1b80ef40915f
HelixFix <jerome.rinner@gmail.com>
2020-04-11 16:11:24 +0200

    fetchAPI

--- resources/assets/js/components/Articles.vue
+++ resources/assets/js/components/Articles.vue
@@ -3,3 +3,39 @@
         <h2>Articles</h2>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            articles: [],
+            article:{
+                id: '',
+                title: '',
+                bodt: ''
+            }, 
+            article_id: '',
+            pagination: {},
+            edit: false        
+        }
+    },
+
+    created() { 
+        this.fetchArticles(); // When it loads we want to run a method called fetch articles
+    },
+
+    // Tips : un objet à l'intérieur d'un tableau s'appel une data
+
+    methods: {
+        fetchArticles() {
+            fetch('api/articles')
+            .then(res => res.json())
+            .then(res => {
+                console.log(res.data);
+
+            });
+        }
+    }
+}
+
+</script>
